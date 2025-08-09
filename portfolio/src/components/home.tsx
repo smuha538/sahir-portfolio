05a811ee4bd9a63454c9c61051bcce386e615e56
smuha538 <sahirbaloch10@gmail.com>
2025-08-08 23:28:09 -0600

    test

--- portfolio/src/components/home.tsx
+++ portfolio/src/components/home.tsx
@@ -7,11 +7,11 @@ function Home() {
           <div className="text-center z-10 px-4">
             <RevealUp>
               <h1 className="text-5xl md:text-7xl mb-6 bg-gradient-to-r from-yellow-300 to-orange-400 bg-clip-text text-transparent leading-right outfit-title">
-                Hey, {<br></br>} I'm Sahir Muhammad Tariq
+                Hey, {<br></br>} I'm Sahir Muhammad Tariqqqq
               </h1>
             </RevealUp>
             <RevealUp>
-              <p className="text-lg mb-8 max-w-lg mx-auto outfit-text">
+              <p className="text-lg mb-8 max-w-lg mx-auto outfit-text text-white">
                 I’m a graduate from Mount Royal University with a degree in Computer Information Systems. <br></br>
                 I love learning various technologies and using them to develop projects that drive my passion and 
                 hope to extend it to help others.
